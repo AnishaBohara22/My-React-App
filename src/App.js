8c24c7531206300c4bc74c0e5e9529a96d514181
Anisha bohara <excellent022am@gmail.com>
2023-02-12 12:29:00 -0500

    add cartIeam component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import './index.css';
+import CartItem from './CartItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>my-react-app</h1> 
       
+      <CartItem />
     </div>
   );
 }
